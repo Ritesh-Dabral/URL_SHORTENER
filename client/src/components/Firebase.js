f078efbc5461bc4322045bab9c70c080e45e9a3c
Ritesh-Dabral <riteshdabral1@gmail.com>
2020-07-28 08:08:44 +0530

    changing firebase

--- client/src/components/Firebase.js
+++ client/src/components/Firebase.js
@@ -2,14 +2,14 @@ import app from 'firebase/app';
 import "firebase/auth";
 
 var firebaseConfig = {
-    apiKey: process.env.APIKEY,
-    authDomain: process.env.AUTHDOMAIN,
-    databaseURL: process.env.DATABASEURL,
-    projectId: process.env.PROJECTID,
-    storageBucket: process.env.STORAGEBUCKET,
-    messagingSenderId: process.env.MESSAGINGSENDERID,
-    appId: process.env.APPID,
-    measurementId: process.env.MEASUREMENTID
+    apiKey: process.env.APIKEY.toString(),
+    authDomain: process.env.AUTHDOMAIN.toString(),
+    databaseURL: process.env.DATABASEURL.toString(),
+    projectId: process.env.PROJECTID.toString(),
+    storageBucket: process.env.STORAGEBUCKET.toString(),
+    messagingSenderId: process.env.MESSAGINGSENDERID.toString(),
+    appId: process.env.APPID.toString(),
+    measurementId: process.env.MEASUREMENTID.toString()
 };
 
 app.initializeApp(firebaseConfig);
