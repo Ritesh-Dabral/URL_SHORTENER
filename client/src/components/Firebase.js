3d5569bea263c246147cdb3f8fc7b8c2fa3c4148
Ritesh-Dabral <riteshdabral1@gmail.com>
2020-07-28 08:13:24 +0530

    almost finished

--- client/src/components/Firebase.js
+++ client/src/components/Firebase.js
@@ -2,14 +2,14 @@ import app from 'firebase/app';
 import "firebase/auth";
 
 var firebaseConfig = {
-    apiKey: process.env.APIKEY.toString(),
-    authDomain: process.env.AUTHDOMAIN.toString(),
-    databaseURL: process.env.DATABASEURL.toString(),
-    projectId: process.env.PROJECTID.toString(),
-    storageBucket: process.env.STORAGEBUCKET.toString(),
-    messagingSenderId: process.env.MESSAGINGSENDERID.toString(),
-    appId: process.env.APPID.toString(),
-    measurementId: process.env.MEASUREMENTID.toString()
+    apiKey: "AIzaSyBfOyPe8LrkD_7u4y27IHdeZV_oauv8tXE",
+    authDomain: "shortify-8debc.firebaseapp.com",
+    databaseURL: "https://shortify-8debc.firebaseio.com",
+    projectId: "shortify-8debc",
+    storageBucket: "shortify-8debc.appspot.com",
+    messagingSenderId: "409334062945",
+    appId: "1:409334062945:web:62d912e97899eb7facef20",
+    measurementId: "G-31G35G4FJF"
 };
 
 app.initializeApp(firebaseConfig);
